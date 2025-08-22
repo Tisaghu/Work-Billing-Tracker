bb317e7dc99e910654fc09d9aa2afd5390605e05
Tisaghu <samasher9@hotmail.com>
2025-08-22 09:10:50 -0700

    Initial setup of settings dialog

---
+++ settings_dialog.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import (
+        QDialog
+)
+
+
+class SettingsDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Settings")
+        self.resize(300, 300)
+
+        #Widgets I need:
+        # Lunch duration : 
+        # Daily Minutes Goal:
+        
+
+
+
+        
+# Temporary for building the window 
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
+    import sys
+    app = QApplication(sys.argv)
+    dlg = SettingsDialog()
+    dlg.exec_()
