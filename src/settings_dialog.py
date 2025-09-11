80b29ede10a4ab06f6f02c11f1cc26efe5e9202b
Tisaghu <samasher9@hotmail.com>
2025-09-11 15:13:04 -0700

    Add 'Daily Minutes' and 'Lunch Duration' entries to the settings dialog

--- src/settings_dialog.py
+++ src/settings_dialog.py
@@ -1,6 +1,13 @@
 from PyQt5.QtWidgets import (
-        QDialog
+         QDialog, QSpinBox, QVBoxLayout, QHBoxLayout, QLabel
 )
+
+from PyQt5.QtCore import Qt
+
+#Constants
+MINUTES_LABEL_TEXT = "Min."
+DAILY_GOAL_LABEL_TEXT = "Daily Goal Minutes:"
+LUNCH_DURATION_LABEL_TEXT = "Lunch Duration:"
 
 
 class SettingsDialog(QDialog):
@@ -9,11 +16,38 @@ class SettingsDialog(QDialog):
         self.setWindowTitle("Settings")
         self.resize(300, 300)
 
-        # Widgets I need:
-        # Lunch duration : 
-        # Daily Minutes Goal:
+        self.dialog_layout = QVBoxLayout()
+        self.setLayout(self.dialog_layout)
+
+        #Daily minutes goal
+        self.daily_goal_layout = QHBoxLayout()
+        self.daily_goal_label = QLabel(DAILY_GOAL_LABEL_TEXT)
+        self.daily_goal_box = QSpinBox()
+        self.daily_goal_minutes_label = QLabel(MINUTES_LABEL_TEXT)
+
+        self.daily_goal_layout.addWidget(self.daily_goal_label)
+        self.daily_goal_layout.addWidget(self.daily_goal_box)
+        self.daily_goal_layout.addWidget(self.daily_goal_minutes_label)
+
+
+        #Lunch duration settings
+        self.lunch_duration_layout = QHBoxLayout()
+        self.lunch_duration_label = QLabel(LUNCH_DURATION_LABEL_TEXT)
+        self.lunch_duration_box = QSpinBox()
+        self.lunch_duration_minutes_label = QLabel(MINUTES_LABEL_TEXT)
+
+        self.lunch_duration_layout.addWidget(self.lunch_duration_label)
+        self.lunch_duration_layout.addWidget(self.lunch_duration_box)
+        self.lunch_duration_layout.addWidget(self.lunch_duration_minutes_label)
+
+
+        #Add layouts to the main dialog layout
+        self.dialog_layout.setAlignment(Qt.AlignTop)
+        self.dialog_layout.addLayout(self.daily_goal_layout)
+        self.dialog_layout.addLayout(self.lunch_duration_layout)
         
 
+    
 
 
         
